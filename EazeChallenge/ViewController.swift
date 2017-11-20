83a58a9e9042e835c890095fcb14c1f01b208284
Apoorv Saini <apoorvsaini9@gmail.com>
2017-11-19 19:48:53 -0500

    Added Strings and Keys property list + Bug Fixes

--- EazeChallenge/ViewController.swift
+++ EazeChallenge/ViewController.swift
@@ -9,40 +9,50 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
     lazy var contentArea = UIView()
     lazy var searchBox = UITextField()
     lazy var tableView = UITableView()
+    var stringKeys: NSDictionary?
+    private let myArray: NSArray = ["First","Second","Third"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let constraint = ConstraintSheet()
         let superview = self.view
         superview?.backgroundColor = UIColor.black
-        
-        /*-------------------------------
+        if let path = Bundle.main.path(forResource: "Strings", ofType: "plist") {
+            stringKeys = NSDictionary(contentsOfFile: path)
+        }
+        /*---------------------------------------------------------
          Create the search text field
-         ---------------------------------*/
+        ---------------------------------------------------------*/
         superview?.addSubview(searchBox)
         searchBox.textAlignment = NSTextAlignment.center
         searchBox.backgroundColor = UIColor.black
         searchBox.textColor = UIColor.white
-        searchBox.attributedPlaceholder = NSAttributedString(string: "SEARCH A GIF",attributes:[NSAttributedStringKey.foregroundColor: UIColor.white])
+        searchBox.attributedPlaceholder = NSAttributedString(string: (stringKeys!["search_prompt"] as? String)!,attributes:[NSAttributedStringKey.foregroundColor: UIColor.white])
         constraint.setSearchBox(searchBox: searchBox, superview: superview!)
         
-        /*-------------------------------
+        /*---------------------------------------------------------
          Create the view holder
-         ---------------------------------*/
+        ----------------------------------------------------------*/
         superview?.addSubview(contentArea)
         contentArea.backgroundColor = UIColor.white
         constraint.setContentArea(contentArea: contentArea,superview: superview!)
         
-        /*-------------------------------
-         Create the Table View for search
-         results
-         ---------------------------------*/
+        /*---------------------------------------------------------
+         Create the Table View for search results
+        ----------------------------------------------------------*/
         contentArea.addSubview(tableView)
+        tableView.separatorColor = UIColor.clear
         constraint.setTableArea(tableView: tableView,superview: contentArea)
+        
+        /*---------------------------------------------------------
+         Create the Table Cell View for search results
+        ----------------------------------------------------------*/
+        
+        
         
     }
 
